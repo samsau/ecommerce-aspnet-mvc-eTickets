2673ec4a16a71dbaf84226c9774f74c9fb4bc0b4
Saurabh Kumar <1355saurabh@gmail.com>
2023-11-20 09:43:59 +0530

    Models, Enums created.

--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -1,7 +1,16 @@
+using eTickets.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(
+    //options =>
+    //{
+    //    options.UseInMemoryDatabase("");
+    //}    
+);
 
 var app = builder.Build();
 
